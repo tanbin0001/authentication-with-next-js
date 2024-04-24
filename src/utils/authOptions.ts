887f8ce29f130370c62b848353193440080d19eb
Ashrafuj Jaman Tanbin <ashrafuj.jaman.tanbin1@gmail.com>
2024-04-24 14:23:57 -0600

    next auth setup done

---
+++ src/utils/authOptions.ts
@@ -0,0 +1,11 @@
+import  { NextAuthOptions } from "next-auth"
+import GithubProvider from "next-auth/providers/github"
+
+export const authOptions:NextAuthOptions = {
+  // Configure one or more authentication providers
+  providers: [
+   
+  ],
+}
+
+ 

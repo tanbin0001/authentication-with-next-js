91c79fe1ec0d33ddb78fda9ec49373a1629726f0
Ashrafuj Jaman Tanbin <ashrafuj.jaman.tanbin1@gmail.com>
2024-04-24 15:02:48 -0600

    added github auth provider

--- src/app/api/auth/[...nextAuth]/route.ts
+++ src/app/api/auth/[...nextAuth]/route.ts
@@ -4,3 +4,7 @@ import NextAuth from "next-auth"
 const handler = NextAuth(authOptions)
 
 export { handler as GET, handler as POST }
+
+
+
+ 
--- src/utils/authOptions.ts
+++ src/utils/authOptions.ts
@@ -4,7 +4,10 @@ import GithubProvider from "next-auth/providers/github"
 export const authOptions:NextAuthOptions = {
   // Configure one or more authentication providers
   providers: [
-   
+    GithubProvider({
+        clientId: process.env.GITLAB_CLIENT_ID  as string,
+        clientSecret: process.env.GITLAB_CLIENT_SECRET as string
+      })
   ],
 }
 
